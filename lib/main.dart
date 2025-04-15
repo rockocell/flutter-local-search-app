cb1bc950c7c586077221170af5eec4d010492708
MJ <gwang907@gmail.com>
2025-04-15 20:35:39 +0900

    design: HomePage layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_search_app/ui/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(home: HomePage());
   }
 }
