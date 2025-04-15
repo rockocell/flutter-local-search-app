cc0071a0afcd96f4013db01019dcdc89d82d9deb
MJ <gwang907@gmail.com>
2025-04-15 19:39:03 +0900

    first commit

---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Scaffold(body: Text('HomePage'));
+  }
+}
