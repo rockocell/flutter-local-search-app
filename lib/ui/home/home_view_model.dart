98ecdcee9ddd62aad7034c628d8ade8eff2707c0
MJ <gwang907@gmail.com>
2025-04-15 21:11:58 +0900

    feat: set home_view_model

--- lib/ui/home/home_view_model.dart
+++ lib/ui/home/home_view_model.dart
@@ -1,1 +1,24 @@
+import 'package:flutter_local_search_app/data/model/local.dart';
+import 'package:flutter_local_search_app/data/repository/local_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+class HomeState {
+  HomeState({required this.locals});
+  List<Local>? locals;
+}
+
+class HomeViewModel extends Notifier<HomeState> {
+  @override
+  HomeState build() {
+    return HomeState(locals: null);
+  }
+
+  Future<void> search(String query) async {
+    LocalRepository localRepository = LocalRepository();
+    state = HomeState(locals: await localRepository.search(query));
+  }
+}
+
+final homeViewModelProvider = NotifierProvider<HomeViewModel, HomeState>(() {
+  return HomeViewModel();
+});
