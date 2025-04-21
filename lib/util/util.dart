1655641470926417a7c83509aeb1439ebbadeb5b
MJ <gwang907@gmail.com>
2025-04-21 14:58:47 +0900

    remove: delete simplifyAddress

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -2,18 +2,3 @@ String removeHtmlTags(String htmlText) {
   final regex = RegExp(r'<[^>]*>', multiLine: true);
   return htmlText.replaceAll(regex, '');
 }
-
-String simplifyAddress(String raw) {
-  final parts = raw.split(' ');
-  for (var part in parts) {
-    if (part.contains('동')) {
-      return part;
-    } // (ㅇㅇ도) ㅇㅇ시 ㅇㅇ구 ㅇㅇ동 ... 에서 'ㅇㅇ동' 사용
-    else if (part.contains('읍')) {
-      return part;
-    } else if (part.contains('면')) {
-      return part;
-    }
-  }
-  return raw;
-}
